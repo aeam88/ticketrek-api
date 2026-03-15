dc4ecada4bfbc1828a2a74f6a243991fda3251f8
Alejandro <a.alvarezmontes@gmail.com>
2026-03-15 09:43:50 -0300

    :sparkles: Implementing Distributed Rate Limiting with Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,15 +9,30 @@ import { EventsModule } from './events/events.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { PaymentsModule } from './payments/payments.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 100,
-    }]),
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const redisHost = config.get<string>('REDIS_HOST', 'localhost');
+        const redisPort = config.get<number>('REDIS_PORT', 6379);
+        const redisUrl = `redis://${redisHost}:${redisPort}`;
+
+        return {
+          throttlers: [
+            {
+              ttl: 60000,
+              limit: 100,
+            },
+          ],
+          storage: new ThrottlerStorageRedisService(redisUrl),
+        };
+      },
+    }),
     PrismaModule,
     AuthModule,
     UsersModule,
