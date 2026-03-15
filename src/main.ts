684e6ec8d33cb836c9c33cad056dffb348e85d57
Alejandro <a.alvarezmontes@gmail.com>
2026-03-15 09:21:56 -0300

    Helmet configuration

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  app.use(helmet());
 
   // Configuración de Swagger
   const config = new DocumentBuilder()
